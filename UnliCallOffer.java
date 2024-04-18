8aaddf98fdc52d168b1f8177fb75b29a3b4b4d0d
Aeron Red Celajes <142370807+AeronRedCelajes@users.noreply.github.com>
2024-04-18 13:51:54 +0800

    visitor pattern code here

--- UnliCallOffer.java
+++ UnliCallOffer.java
@@ -1,4 +1,4 @@
-interface UnliCallOffer {
+interface UnliCallsTextOffer {
     public String showUnliCallsTextOffer(String telcoName, boolean unliCallText);
     public String showUnliCallsTextOffer(TelcoSubscription telco); 
 }
