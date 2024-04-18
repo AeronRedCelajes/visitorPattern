622ff89d98a5f5b522a2c7fd2f87ee332c9adcec
Aeron Red Celajes <142370807+AeronRedCelajes@users.noreply.github.com>
2024-04-18 16:07:57 +0800

    Delete visitorPattern/UnliCallOffer.java

--- visitorPattern/UnliCallOffer.java
+++
@@ -1,6 +0,0 @@
-package visitorPattern;
-
-interface UnliCallsTextOffer {
-    public String showUnliCallsTextOffer(String telcoName, boolean unliCallText);
-    public String showUnliCallsTextOffer(TelcoSubscription telco); 
-}
